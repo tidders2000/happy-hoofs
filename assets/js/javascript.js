fa7428e37119419f8d38d64aed97df4679e07056
Simon Tidman <simon.tid@btinternet.com>
2018-07-16 19:03:21 +0000

    js file added to fix button w3c issue

---
+++ assets/js/javascript.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+	
+
+
+
+$("#beach").click(function() {
+	
+	window.location.href="groups.html";
+
+});
+
+$("#lessons").click(function() {
+	
+	window.location.href="lessons.html";
+
+});
+
+$("#livery").click(function() {
+	
+	window.location.href="livery.html";
+
+});
+
+
+});
